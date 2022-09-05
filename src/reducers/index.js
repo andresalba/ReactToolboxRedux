b9056e1fa68826df6d9d4174ab2752d6cca07dcf
Andres Alba <tensinkusum@example.com>
2022-09-04 23:12:27 -0500

    recat redux

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import counterReducer from "./counter";
+import loggerReducer from "./isLogged";
+import { combineReducers } from "redux";
+
+const allReducers = combineReducers({
+    counter: counterReducer,
+    isLogged: loggerReducer
+})
+
+export default allReducers;
